98d63252f99b9ed6b014ea8b7c4fa8abfbe35a66
Jean Malm <jean.malm@mdh.se>
2022-04-26 10:11:21 +0200

    [test][feat] Add initial tests for post-analyzer for test code
    * Started development of scaffolding for post-analysis on test code.
    * Aim is to handle false-positives and refute wrong analysis assumptions.

---
+++ analyzer/tools/testcode_analyzer/test/test_assertion_classifier.py
@@ -0,0 +1,27 @@
+import pytest
+from ..src.result_parsing import parse_result_file, raised_on_assertion, ResultReport
+
+
+def test_parse_plist_result_missing_file():
+    """[Negative]: Does function handle invalid files"""
+    invalid_file = parse_result_file('./testdata/not_a_valid_file.plist')
+    assert invalid_file is None
+
+def test_parse_plist_result_existing():
+    result = parse_result_file('./testdata/has_assertion.plist')
+    assert result is not None
+    assert isinstance(result, ResultReport)
+
+def test_plist_report_data():
+    result = parse_result_file('./testdata/has_one_report.plist')
+    assert result.report_amount == 1
+    assert result.files == ['./testdata/has_assertion.cpp']
+
+
+def test_plist_report_diagnostics():
+    result = parse_result_file('./testdata/has_one_report.plist')
+    first_report = result.reports[0]
+    ROW_NR = 238
+    COL_NR = 3
+    assert first_report.check_name == 'core.NullDereference'
+    assert first_report.location == (0, ROW_NR, COL_NR)
